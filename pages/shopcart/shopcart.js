5a0b86c442d53602a6a028ea47cd56662b6f49a1
LinhuHe <2351855940@qq.com>
2020-04-08 17:07:35 +0800

    购物车完成

--- pages/shopcart/shopcart.js
+++ pages/shopcart/shopcart.js
@@ -5,7 +5,53 @@ Page({
    * 页面的初始数据
    */
   data: {
+    shopers:[
+      "shea",
+      "woshimaijia",
+      "biroie"
+    ],
+    shopCartData:[
+      [
+        {shoper:"shea",did:11,rid:1,name:"warhsssdeqgsdgr官网如果风EGHFDQWgagha",src:"../../picture/colSelect.png",price:85.25,stock:852,color:"绿色",size:"L",isOnSale:"1",style:""},
+        {shoper:"shea",did:21,rid:2,name:"gay",src:"../../picture/colSelect.png",price:14.25,stock:352,color:"绿色",size:"L",isOnSale:"1",style:""}
+      ],
+      [
+        {shoper:"woshimaijia",did:31,rid:3,name:"asfy",src:"../../picture/colSelect.png",price:14.25,stock:52,color:"绿色",size:"L",isOnSale:"0",style:""},
+        {shoper:"woshimaijia",did:41,rid:4,name:"nnay",src:"../../picture/colSelect.png",price:114.25,stock:342,color:"绿色",size:"L",isOnSale:"1",style:""},
+        {shoper:"woshimaijia",did:51,rid:5,name:"utejay",src:"../../picture/colSelect.png",price:8814.25,stock:852,color:"绿色",size:"L",isOnSale:"1",style:""}
+      ],
+      [
+        {shoper:"biroie",did:6,rid:61,name:"t134qy",src:"../../picture/colSelect.png",price:153.25,stock:356,color:"绿色",size:"L",isOnSale:"1",style:""},
+        {shoper:"biroie",did:7,rid:71,name:"j75wsr",src:"../../picture/colSelect.png",price:214.25,stock:3554,color:"绿色",size:"L",isOnSale:"1",style:""}
+      ]
+    ],
+    totalPrice:0
+  },
 
+  selected(e)
+  {
+    var sum=0;
+    for(var i=0;i<e.detail.value.length;i++)
+    {
+      sum+=Number(e.detail.value[i]);
+    }
+    this.setData({
+      totalPrice:sum
+    })
+
+    console.log("checked price is:"+e.detail.value+"\n"+"total is:"+this.data.totalPrice)
+  },
+
+  tapOnGood(e)
+  {
+    console.log(e.currentTarget.dataset.rid)
+    wx.navigateTo({
+      url: '../goodsDetail/goodsDetail?rid='+e.currentTarget.dataset.rid
+    })
+  },
+  removeShopcart(e)
+  {
+    console.log("removeed did is:"+e.currentTarget.dataset.did)
   },
 
   /**
